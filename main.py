b4564c57dff354666298f54f3f880892af103906
Eang Hengty <hengty174@gmail.com>
2025-04-27 16:28:33 +0700

    [minor update on gitignore file]

--- main.py
+++ main.py
@@ -8,10 +8,27 @@ except ModuleNotFoundError:
     subprocess.check_call([sys.executable
                            ,"-m","pip","install","yt-dlp"])
     import yt_dlp #try again after install
+import shutil
+import os
+
+# 📌 check if ffmpeg install
+def find_ffmpeg_folder():
+    ffmpeg_path=shutil.which("ffmpeg")
+    ffprobe_path=shutil.which("ffprobe")
+    if ffmpeg_path and ffprobe_path:
+        # ✅ Return folder path, not file path
+        return os.path.dirname(ffmpeg_path)
+    else:
+        print("⚠️ Ffmpeg not found! please install it to get the best quality download.")
+        print("You can download it here: https://www.gyan.dev/ffmpeg/builds/")
+        return None
+        
 
 # 📌 main function to download the video
 def download_video():
     url = input("Enter the url: ")
+
+    ffmpeg_folder=find_ffmpeg_folder() # 📌 Detect ffmpeg path automatically
 
     print("\nChoose quality:")
     print("1. Best quality (default)")
@@ -29,7 +46,8 @@ def download_video():
                 'preferredcodec':'mp3',
                 'preferredquality':'192',
             }],
-            'outtmpl':'%(title)s.%(ext)s'
+            'outtmpl':'%(title)s.%(ext)s',
+            
         }
     elif choice =="3":
 
@@ -37,16 +55,20 @@ def download_video():
             'format':'bestvideo[height<=720]+bestaudio/best[height<=720]',
             'outtmpl':'%(title)s.%(ext)s',
             'merge_output_format': 'mp4',
+            
         }
     else: # default best quality it can get
         yt_opts={
             'format':'bestvideo+bestaudio/best',
             'outtmpl':'%(title)s.%(ext)s',
             'merge_output_format': 'mp4',
+            
 
         }
 
-    yt_opts={} # you can add option here like quality
+    # 📌 If ffmpeg is found, tell yt-dlp where it is
+    if ffmpeg_folder:
+        yt_opts['ffmpeg_location'] = ffmpeg_folder
     with yt_dlp.YoutubeDL(yt_opts) as ydl:
         ydl.download([url])
 # start the program
