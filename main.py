3853e4400d665977ff96468dbef1a1a2c9c531fc
Eang Hengty <hengty174@gmail.com>
2025-04-27 15:21:30 +0700

    [add quality option]

--- main.py
+++ main.py
@@ -13,6 +13,38 @@ except ModuleNotFoundError:
 def download_video():
     url = input("Enter the url: ")
 
+    print("\nChoose quality:")
+    print("1. Best quality (default)")
+    print("2. audio only (mp3)")
+    print("3. 720p if available")
+    choice = input("Enter your choice (1/2/3): ")
+
+# 📌 setting options best on user choice
+    if choice == "2":
+        yt_opts={
+
+            'format': 'bestaudio/best',
+            'postprocessors':[{
+                'key':'FFmpegExtractAudio',
+                'preferredcodec':'mp3',
+                'preferredquality':'192',
+            }],
+            'outtmpl':'%(title)s.%(ext)s'
+        }
+    elif choice =="3":
+
+        yt_opts={
+            'format':'bestvideo[height<=720]+bestaudio/best[height<=720]',
+            'outtmpl':'%(title)s.%(ext)s',
+            'merge_output_format': 'mp4',
+        }
+    else: # default best quality it can get
+        yt_opts={
+            'format':'bestvideo+bestaudio/best',
+            'outtmpl':'%(title)s.%(ext)s',
+            'merge_output_format': 'mp4',
+
+        }
 
     yt_opts={} # you can add option here like quality
     with yt_dlp.YoutubeDL(yt_opts) as ydl:
